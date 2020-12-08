e2c36699508e8d7d9660a1471b4002a2466f1bff
nadas00 <se.hasanciftci@gmail.com>
2020-12-08 22:00:54 +0300

    App bar added.

--- lib/pages/root_app.dart
+++ lib/pages/root_app.dart
@@ -18,7 +18,31 @@ class _IndexedStackAppState extends State<IndexedStackApp> {
   }
 
   Widget getAppBar() {
-    return AppBar();
+    List appBarIcons = [
+      Icons.add_box_outlined,
+      Icons.favorite_border,
+      Icons.chat_bubble_outline
+    ];
+
+    return AppBar(
+      backgroundColor: Colors.black,
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text('IndexedStackApp', style: TextStyle(color: Colors.white)),
+          Row(
+            children: List.generate(appBarIcons.length, (index) {
+              return IconButton(
+                icon: Icon(appBarIcons[index]),
+                color: Colors.white,
+                onPressed: () {},
+              );
+            }),
+          ),
+        ],
+      ),
+    );
   }
 
   Widget getNavBar() {
