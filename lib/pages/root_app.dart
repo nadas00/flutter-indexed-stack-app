e3b7af6106391dbae423c5d1c7c9579348ba735d
nadas00 <se.hasanciftci@gmail.com>
2020-12-08 21:24:20 +0300

    Custom bottom navigation bar added.

--- lib/pages/root_app.dart
+++ lib/pages/root_app.dart
@@ -6,6 +6,8 @@ class IndexedStackApp extends StatefulWidget {
 }
 
 class _IndexedStackAppState extends State<IndexedStackApp> {
+  var pageIndex;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +22,55 @@ class _IndexedStackAppState extends State<IndexedStackApp> {
   }
 
   Widget getNavBar() {
-    return Container();
+    var size = MediaQuery.of(context).size;
+
+    List navBarItems = [
+      Icons.home_outlined,
+      Icons.search,
+      Icons.movie_filter_outlined,
+      Icons.shopping_bag_outlined,
+    ];
+
+    List activeNavBarItems = [
+      Icons.home_filled,
+      Icons.saved_search,
+      Icons.movie_filter,
+      Icons.shopping_bag,
+    ];
+
+    return Container(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+        child: Container(
+          width: size.width,
+          height: 70.0,
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: List.generate(4, (index) {
+                return IconButton(
+                  icon: index == pageIndex
+                      ? Icon(activeNavBarItems[index])
+                      : Icon(navBarItems[index]),
+                  iconSize: 30.0,
+                  color: Colors.white,
+                  onPressed: () {
+                    setState(() {
+                      pageIndex = index;
+                    });
+                  },
+                );
+              }),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
   Widget getBody() {
