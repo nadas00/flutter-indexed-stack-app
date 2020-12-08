0e1c0e307c4af6c254c78ac0af83473919b47ead
nadas00 <se.hasanciftci@gmail.com>
2020-12-08 20:36:03 +0300

    Missing runApp added to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:tabview_with_custom_navbar/pages/root_app.dart';
 
 void main() {
-  MaterialApp(
-      debugShowCheckedModeBanner:false,
-      home: IndexedStackApp()
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: IndexedStackApp(),
+    ),
   );
 }
-
